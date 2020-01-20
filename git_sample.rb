b1a853bd80c44ddbcec7b1a2c61f09ff7a1e992a
KAZUHIRO <kazuhiro@KAZUHIROnoiMac.local>
2020-01-20 23:11:21 +0900

    git_sampleファイルを追加

---
+++ git_sample.rb
@@ -0,0 +1,21 @@
+puts "hello"
+puts 3 + 4
+
+puts <<~TEXT
+
+こんにちは
+
+具志です
+
+宜しくお願いします
+
+SELECT = FROM USERS;
+
+TEXT
+
+
+users = ["satou", "taira", "yamada", "hayashi"]
+
+users.each do |user|
+    puts user
+end
